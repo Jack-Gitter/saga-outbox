61d93033f8d554c50e754348e559164adc6c53ce
Jack Gitter <jack.gitter@gmail.com>
2025-04-18 16:38:31 -0400

    nice

--- orders/src/orders/rabbitmq/rabbitmq.module.ts
+++ orders/src/orders/rabbitmq/rabbitmq.module.ts
@@ -6,21 +6,25 @@ const providers: Provider[] = [
   {
     provide: RabbitMQService,
     useFactory: async () => {
-      const inventory_queue = 'INVENTORY_CHANNEL';
-      const shipping_queue = 'SHIPPING_CHANNEL';
-      const inventory_queue_resp = 'INVENTORY_CHANNEL_RESP';
-      const shipping_queue_resp = 'SHIPPING_CHANNEL_RESP';
+      const inventory_reserve_queue = 'INVENTORY_RESERVER_QUEUE';
+      const shipping_queue = 'SHIPPING_QUEUE';
+      const inventory_reserve_queue_resp = 'INVENTORY_RESERVE_QUEUE_RESP';
+      const shipping_queue_resp = 'SHIPPING_QUEUE_RESP';
+      const inventory_delete_queue = 'INVENTORY_DELETE_QUEUE';
+      const inventory_delete_queue_resp = 'INVENTORY_DELETE_QUEUE_RESP';
       const connection = new amqp.Connection('amqp://localhost');
       await connection.init();
       const channel = await connection.createChannel();
-      await channel.assertQueue(inventory_queue);
+      await channel.assertQueue(inventory_reserve_queue);
       await channel.assertQueue(shipping_queue);
       return new RabbitMQService(
         channel,
-        inventory_queue,
+        inventory_reserve_queue,
         shipping_queue,
-        inventory_queue_resp,
+        inventory_reserve_queue_resp,
         shipping_queue_resp,
+        inventory_delete_queue,
+        inventory_delete_queue_resp,
       );
     },
   },
--- orders/src/orders/rabbitmq/rabbitmq.orders.ts
+++ orders/src/orders/rabbitmq/rabbitmq.orders.ts
@@ -5,18 +5,23 @@ Injectable();
 export class RabbitMQService {
   constructor(
     private channel: amqp.Channel,
-    private inventory_queue: string,
+    private inventory_reserve_queue: string,
     private shipping_queue: string,
-    private inventory_queue_resp: string,
+    private inventory_reserve_queue_resp: string,
     private shipping_queue_resp: string,
+    private inventory_delete_queue: string,
+    private inventory_delete_queue_resp: string,
   ) {}
 
   async sendInventoryReserveMessage(product: number, quantity: number) {
     await this.channel.sendToQueue(
-      this.inventory_queue,
+      this.inventory_reserve_queue,
       Buffer.from(JSON.stringify({ product, quantity })),
     );
-    await this.channel.consume(this.inventory_queue_resp, this.handleResponse);
+    await this.channel.consume(
+      this.inventory_reserve_queue_resp,
+      this.handleResponse,
+    );
   }
 
   async sendShippingMessage(product: number, quantity: number) {
@@ -27,10 +32,15 @@ export class RabbitMQService {
     await this.channel.consume(this.shipping_queue_resp, this.handleResponse);
   }
 
+  async sendInventoryDeleteMessage(product: number, quantity: number) {
+    await this.channel.sendToQueue(this.inventory_reserve_queue, content);
+  }
+
   private handleResponse(mes: amqp.Message) {
     const content = JSON.parse(mes.content.toString());
     if (content.success !== true) {
       throw new Error(content.message);
     }
+    console.debug(`step succeeded! ${content.message}`);
   }
 }
