d962cc78e5db03bf2d2cdb6a2e992af867854f64
Jack Gitter <jack.gitter@gmail.com>
2025-04-19 09:56:10 -0400

    nice

--- orders/src/orders/saga/orders.orchestrator.ts
+++ orders/src/orders/saga/orders.orchestrator.ts
@@ -8,7 +8,6 @@ export class OrdersSagaOrchestrator
   implements ISagaOrchestrator<ORDERS_SAGA_STEP>
 {
   constructor(private steps: Map<ORDERS_SAGA_STEP, ISagaStep>) {}
-  invokedSteps: ORDERS_SAGA_STEP[] = [];
 
   async invokeStep(step: ORDERS_SAGA_STEP): Promise<void> {
     const currentStep = this.steps.get(step);
@@ -16,15 +15,13 @@ export class OrdersSagaOrchestrator
       throw new InternalServerErrorException(`Invalid Step!`);
     }
     currentStep.invoke();
-    this.invokedSteps.push(step);
   }
 
-  async compensate(): Promise<void> {
-    const executedSteps = this.invokedSteps.map((invoked) => {
-      return this.steps.get(invoked);
-    });
-    executedSteps.forEach((step) => {
-      step.rollback();
-    });
+  compensate(step: ORDERS_SAGA_STEP): void {
+    const currentStep = this.steps.get(step);
+    if (!currentStep) {
+      throw new InternalServerErrorException(`Invalid Step!`);
+    }
+    currentStep.rollback();
   }
 }
