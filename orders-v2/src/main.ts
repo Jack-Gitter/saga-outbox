4bd8217491c66172e7874155fbc2090a3e9d9649
Jack Gitter <jack.gitter@gmail.com>
2025-04-19 18:36:31 -0400

    nice

--- orders-v2/src/main.ts
+++ orders-v2/src/main.ts
@@ -1,16 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const microservice = app.connectMicroservice({
+  app.connectMicroservice({
+    transport: Transport.RMQ,
     options: {
       urls: [`amqp://localhost:5672`],
       queue: 'my_queue',
       queueOptions: { durable: false },
     },
-    transport: Transport.
   });
 
   await app.startAllMicroservices();
