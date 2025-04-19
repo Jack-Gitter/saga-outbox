5cc6f6d4e9d9e2994250a00761639b671660fdb8
Jack Gitter <jack.gitter@gmail.com>
2025-04-18 20:33:05 -0400

    nice

---
+++ orders/src/orders/rabbitmq/queue.types.ts
@@ -0,0 +1,12 @@
+export const inventory_reserve_queue = 'INVENTORY_RESERVER_QUEUE';
+export const inventory_reserve_queue_resp = 'INVENTORY_RESERVE_QUEUE_RESP';
+export const inventory_delete_queue = 'INVENTORY_DELETE_QUEUE';
+export const inventory_delete_queue_resp = 'INVENTORY_DELETE_QUEUE_RESP';
+export const inventory_reserve_rollback_queue =
+  'INVENTORY_RESERVE_ROLLBACK_QUEUE';
+export const inventory_reserve_rollback_queue_resp =
+  'INVENTORY_RESERVE_ROLLBACK_QUEUE';
+export const shipping_queue = 'SHIPPING_QUEUE';
+export const shipping_queue_resp = 'SHIPPING_QUEUE_RESP';
+export const shipping_rollback_queue = 'SHIPPING_ROLLBACK_QUEUE';
+export const shipping_rollback_queue_resp = 'SHIPPING_ROLLBACK_QUEUE_RESP';
--- orders/src/orders/rabbitmq/rabbitmq.module.ts
+++ orders/src/orders/rabbitmq/rabbitmq.module.ts
@@ -1,23 +1,23 @@
 import { Module, Provider } from '@nestjs/common';
 import { RabbitMQService } from './rabbitmq.orders';
 import * as amqp from 'amqplib-as-promised';
+import {
+  inventory_delete_queue,
+  inventory_delete_queue_resp,
+  inventory_reserve_queue,
+  inventory_reserve_queue_resp,
+  inventory_reserve_rollback_queue,
+  inventory_reserve_rollback_queue_resp,
+  shipping_queue,
+  shipping_queue_resp,
+  shipping_rollback_queue,
+  shipping_rollback_queue_resp,
+} from './queue.types';
 
 const providers: Provider[] = [
   {
     provide: RabbitMQService,
     useFactory: async () => {
-      const inventory_reserve_queue = 'INVENTORY_RESERVER_QUEUE';
-      const inventory_reserve_queue_resp = 'INVENTORY_RESERVE_QUEUE_RESP';
-      const inventory_delete_queue = 'INVENTORY_DELETE_QUEUE';
-      const inventory_delete_queue_resp = 'INVENTORY_DELETE_QUEUE_RESP';
-      const inventory_reserve_rollback_queue =
-        'INVENTORY_RESERVE_ROLLBACK_QUEUE';
-      const inventory_reserve_rollback_queue_resp =
-        'INVENTORY_RESERVE_ROLLBACK_QUEUE';
-      const shipping_queue = 'SHIPPING_QUEUE';
-      const shipping_queue_resp = 'SHIPPING_QUEUE_RESP';
-      const shipping_rollback_queue = 'SHIPPING_ROLLBACK_QUEUE';
-      const shipping_rollback_queue_resp = 'SHIPPING_ROLLBACK_QUEUE_RESP';
       const connection = new amqp.Connection('amqp://localhost');
       await connection.init();
       const channel = await connection.createChannel();
@@ -27,6 +27,10 @@ const providers: Provider[] = [
       await channel.assertQueue(shipping_queue_resp);
       await channel.assertQueue(inventory_delete_queue);
       await channel.assertQueue(inventory_reserve_queue_resp);
+      await channel.assertQueue(inventory_reserve_rollback_queue);
+      await channel.assertQueue(inventory_reserve_rollback_queue_resp);
+      await channel.assertQueue(shipping_rollback_queue);
+      await channel.assertQueue(shipping_rollback_queue_resp);
       return new RabbitMQService(
         channel,
         inventory_reserve_queue,
