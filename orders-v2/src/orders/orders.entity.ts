50b6bca3d3f62dc4966dfe32c96dc522bc624d12
Jack Gitter <jack.gitter@gmail.com>
2025-04-19 18:13:42 -0400

    nice

---
+++ orders-v2/src/orders/orders.entity.ts
@@ -0,0 +1,27 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { STATUS } from './orders.enums';
+
+@Entity('Orders')
+export class Order {
+  constructor(product: number, quantity: number) {
+    this.product = product;
+    this.quantity = quantity;
+  }
+
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'int', nullable: false })
+  product: number;
+
+  @Column({ type: 'int', nullable: false })
+  quantity: number;
+
+  @Column({
+    type: 'enum',
+    enum: STATUS,
+    nullable: false,
+    default: STATUS.PENDING,
+  })
+  status: STATUS;
+}
---
+++ orders-v2/src/orders/orders.outbox.entity.ts
@@ -0,0 +1,29 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('OrdersOutboxMessages')
+export class OrdersOutboxMessage {
+  constructor(product: number, quantity: number, orderId: number) {
+    this.product = product;
+    this.quantity = quantity;
+    this.orderId = orderId;
+  }
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'int', nullable: false })
+  orderId: number;
+
+  @Column({ type: 'int', nullable: false })
+  product: number;
+
+  @Column({ type: 'int', nullable: false })
+  quantity: number;
+
+  public toJSON() {
+    return {
+      orderId: this.orderId,
+      product: this.product,
+      quantity: this.quantity,
+    };
+  }
+}
