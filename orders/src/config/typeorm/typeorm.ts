45341cad5b0dfa57c2f4efa172eec2cc2358ff63
Jack Gitter <jack.gitter@gmail.com>
2025-04-19 13:15:27 -0400

    nice

--- orders/src/config/typeorm/typeorm.ts
+++ orders/src/config/typeorm/typeorm.ts
@@ -2,6 +2,7 @@ import { Order } from '../../orders/orders.entity';
 import { OrdersOutboxMessage } from '../../orders/orders.outbox.entity';
 import { DataSource } from 'typeorm';
 import { Init1744937663214 } from './migrations/1744937663214-Init';
+import { Init1745082901532 } from './migrations/1745082901532-Init';
 
 export const dataSource: DataSource = new DataSource({
   type: 'postgres',
@@ -11,5 +12,5 @@ export const dataSource: DataSource = new DataSource({
   password: 'postgres',
   database: 'postgres',
   entities: [Order, OrdersOutboxMessage],
-  migrations: [Init1744937663214],
+  migrations: [Init1744937663214, Init1745082901532],
 });
