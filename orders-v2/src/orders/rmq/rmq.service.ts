464c5d5590241a53e8c3bc8b2fafca8a4c520e3a
Jack Gitter <jack.gitter@gmail.com>
2025-04-19 21:07:27 -0400

    nice

--- orders-v2/src/orders/rmq/rmq.service.ts
+++ orders-v2/src/orders/rmq/rmq.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 import { Channel, Message } from 'amqplib-as-promised/lib';
 import { OrdersOutboxMessage } from '../orders.outbox.entity';
 import { INVENTORY_RESERVE } from '../orders.symbols';
-import { INVENTORY_RESERVE_RESPONSE, SHIPPING_VALIDATION } from './rmq.types';
+import {
+  INVENTORY_RESERVE_RESPONSE,
+  SHIPPING_VALIDATION,
+  SHIPPING_VALIDATION_RESPONSE,
+} from './rmq.types';
 import { MessageResponse } from '../orders.types';
 
 @Injectable()
@@ -38,6 +42,22 @@ export class RMQService {
     await this.channel.sendToQueue(
       SHIPPING_VALIDATION,
       Buffer.from(JSON.stringify(mes.toJSON())),
+    );
+  }
+
+  async registerShippingValidationMessageResponseHandler(
+    fun: (messageResponse: MessageResponse) => unknown,
+  ) {
+    await this.channel.consume(
+      SHIPPING_VALIDATION_RESPONSE,
+      async (mes: Message) => {
+        const contents = JSON.parse(mes.content.toString());
+        const messageResponse: MessageResponse = {
+          successful: contents.successful,
+          orderId: contents.orderId,
+        };
+        await fun(messageResponse);
+      },
     );
   }
 }
