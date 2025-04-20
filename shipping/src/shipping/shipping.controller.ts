32bc70ec2889dc8f0d8cf4d7981eac69b5ca6a66
Jack Gitter <jack.gitter@gmail.com>
2025-04-20 17:41:46 -0400

    nice

--- shipping/src/shipping/shipping.controller.ts
+++ shipping/src/shipping/shipping.controller.ts
@@ -1,9 +1,19 @@
 import { Controller } from '@nestjs/common';
 import { ShippingService } from './shipping.service';
+import { RMQService } from './rmq/rmq.service';
 
 @Controller()
 export class ShippingController {
-  constructor(private shippingService: ShippingService) {}
+  constructor(
+    private shippingService: ShippingService,
+    private rmqService: RMQService,
+  ) {}
 
-  onApplicationBootstrap() {}
+  onApplicationBootstrap() {
+    this.rmqService.registerShippingValidationMessageHandler(
+      this.shippingService.handleShippingValidationMessage.bind(
+        this.shippingService,
+      ),
+    );
+  }
 }
