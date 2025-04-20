17f46f262942b99058b9d0a451384a6c2a08bcf2
Jack Gitter <jack.gitter@gmail.com>
2025-04-20 08:35:26 -0400

    nice

--- inventory/src/inventory/rmq/rmq.service.ts
+++ inventory/src/inventory/rmq/rmq.service.ts
@@ -1,7 +1,17 @@
 import { Injectable } from '@nestjs/common';
-import { Channel } from 'amqplib-as-promised/lib';
+import { Channel, Message } from 'amqplib-as-promised/lib';
+import { INVENTORY_RESERVE } from './rmq.types';
 
 @Injectable()
 export class RMQService {
   constructor(private channel: Channel) {}
+
+  async registerInventoryReserveMessageHandler(
+    fun: (id: number, product: number, quantity: number) => void,
+  ) {
+    this.channel.consume(INVENTORY_RESERVE, async (mes: Message) => {
+      const content = JSON.parse(mes.content.toString());
+      await fun(content.id, content.product, content.quantity);
+    });
+  }
 }
