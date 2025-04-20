a7ef61af6550427f45f20bcfa1a4fe73fce00725
Jack Gitter <jack.gitter@gmail.com>
2025-04-20 17:37:46 -0400

    nice

--- shipping/src/shipping/rmq/rmq.module.ts
+++ shipping/src/shipping/rmq/rmq.module.ts
@@ -1,6 +1,21 @@
 import { Module, Provider } from '@nestjs/common';
+import { RMQService } from './rmq.service';
+import { Connection } from 'amqplib-as-promised/lib';
+import { SHIPPING_VALIDATION, SHIPPING_VALIDATION_RESPONSE } from './rmq.types';
 
-const providers: Provider[] = [];
+const providers: Provider[] = [
+  {
+    provide: RMQService,
+    useFactory: async () => {
+      const connection = new Connection('amqp://localhost');
+      await connection.init();
+      const channel = await connection.createChannel();
+      await channel.assertQueue(SHIPPING_VALIDATION);
+      await channel.assertQueue(SHIPPING_VALIDATION_RESPONSE);
+      return new RMQService(channel);
+    },
+  },
+];
 @Module({
   providers: [...providers],
   exports: [...providers],
