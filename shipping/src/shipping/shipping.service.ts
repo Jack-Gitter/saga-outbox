4ba81bed9dd9d1421717a8f5a8a6e1706ca12ab7
Jack Gitter <jack.gitter@gmail.com>
2025-04-20 17:51:49 -0400

    nice

--- shipping/src/shipping/shipping.service.ts
+++ shipping/src/shipping/shipping.service.ts
@@ -7,11 +7,13 @@ export class ShippingService {
   constructor(private rmqService: RMQService) {}
   async handleShippingValidationMessage(mes: ShippingValidationMessage) {
     if (process.env.ACCEPT_SHIPPING ?? true) {
+      console.debug(`Shipping validation accepted`);
       await this.rmqService.sendShippingValidationResponse({
         orderId: mes.orderId,
         successful: true,
       });
     } else {
+      console.debug(`Shipping validation rejected`);
       await this.rmqService.sendShippingValidationResponse({
         orderId: mes.orderId,
         successful: false,
