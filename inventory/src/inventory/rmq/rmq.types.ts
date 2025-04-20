8b35e11e51fe6083af387130988759a8cb676b46
Jack Gitter <jack.gitter@gmail.com>
2025-04-20 14:47:19 -0400

    nice

--- inventory/src/inventory/rmq/rmq.types.ts
+++ inventory/src/inventory/rmq/rmq.types.ts
@@ -1,7 +1,7 @@
 export const INVENTORY_RESERVE = 'INVENTORY_RESERVE';
-export const INVENTORY_RESERVE_RESP = 'INVENTORY_RESERVE_RESP';
+export const INVENTORY_RESERVE_RESP = 'INVENTORY_RESERVE_RESPONSE';
 export const INVENTORY_REMOVE = 'INVENTORY_REMOVE';
-export const INVENTORY_REMOVE_RESP = 'INVENTORY_REMOVE_RESP';
+export const INVENTORY_REMOVE_RESP = 'INVENTORY_REMOVE_RESPONSE';
 export type InventoryReserveInboxMessage = {
   orderId: number;
   product: number;
