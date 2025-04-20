b25a61572f35515516e63c50adadc7e1e34508dc
Jack Gitter <jack.gitter@gmail.com>
2025-04-19 20:40:19 -0400

    nice

--- orders-v2/src/orders/rmq/rmq.service.ts
+++ orders-v2/src/orders/rmq/rmq.service.ts
@@ -1,7 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { Channel } from 'amqplib-as-promised/lib';
+import { OrdersOutboxMessage } from '../orders.outbox.entity';
+import { INVENTORY_RESERVE } from '../orders.symbols';
 
-@Injectable();
+@Injectable()
 export class RMQService {
-    constructor(private channel: Channel) {}
+  constructor(private channel: Channel) {}
+
+  async sendInventoryReserveMessage(mes: OrdersOutboxMessage) {
+    this.channel.sendToQueue(
+      INVENTORY_RESERVE,
+      Buffer.from(JSON.stringify(mes.toJSON())),
+    );
+  }
 }
