995de91d1cfa96a976b71e9164e297d59ea09c07
Jack Gitter <jack.gitter@gmail.com>
2025-04-20 09:18:30 -0400

    nice

--- inventory/src/inventory/inventory.controller.ts
+++ inventory/src/inventory/inventory.controller.ts
@@ -1,8 +1,19 @@
 import { Controller } from '@nestjs/common';
+import { RMQService } from './rmq/rmq.service';
+import { InventoryService } from './inventory.service';
 
 @Controller()
 export class InventoryController {
+  constructor(
+    private rmqService: RMQService,
+    private inventoryService: InventoryService,
+  ) {}
   onApplicationBoostrap() {
+    this.rmqService.registerInventoryReserveMessageHandler(
+      this.inventoryService.handleInventoryReserveMessage.bind(
+        this.inventoryService,
+      ),
+    );
     // register handlers for incomming inventory reservation and deletion
     // register the poller to poll outbox messages
   }
